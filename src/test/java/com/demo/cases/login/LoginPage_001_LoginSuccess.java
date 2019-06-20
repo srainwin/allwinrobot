5bfcc90a733d3e3b058040345918f91b397727d9
xwr <helloxwr@163.com>
2019-06-20 17:59:27 +0800

    xwr

--- src/test/java/com/demo/cases/login/LoginPage_001_LoginSuccess.java
+++ src/test/java/com/demo/cases/login/LoginPage_001_LoginSuccess.java
@@ -27,16 +27,13 @@ public class LoginPage_001_LoginSuccess extends LoginBase {
 	@Test(dataProvider = "testdata", description = "正确的账号密码登录") // allure用例名，description
 	@Story("登陆正向测试用例") // allure用例组织，三级模块
 	@Description("操作步骤：" + "1、输入用户名和密码，点击登录；" + "预期结果：" + "1、成功登录，且当前用户名信息正确；") // allure用例描述
+	@Step("获取cookies保存，便于其他用例免登录")
 	public void loginSucess(String username, String password, String expect) throws Exception {
 		// 登陆
 		LoginOperation.login(seleniumUtil, username, password, testurl);
 		// 获取cookies保存，便于其他用例免登录
-		cookiesSaveInFile();
+		seleniumUtil.cookiesSaveInFile(cookiesConfigFilePath);
 		// 用户信息断言
 		LoginOperation.assertLoginCurrentUser(seleniumUtil, expect);
-	}
-	@Step("获取cookies保存，便于其他用例免登录")
-	private void cookiesSaveInFile(){
-		seleniumUtil.cookiesSaveInFile(cookiesConfigFilePath);
 	}
 }
