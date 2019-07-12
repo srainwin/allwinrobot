e5210eb43491609949bc3ff7bb746e21b83626b4
xwr <helloxwr@163.com>
2019-07-12 14:14:24 +0800

    xwr

---
+++ src/main/java/com/demo/utils/SikuliWebUtil.java
@@ -0,0 +1,18 @@
+package com.demo.utils;
+
+import org.apache.log4j.Logger;
+
+/**
+ * @author XWR
+ * @Description sikuli-webdriver
+ */
+public class SikuliWebUtil {
+	public static Logger logger = Logger.getLogger(SikuliWebUtil.class.getName());
+	
+	public void launchScreen(String sikuliImageFolderPath){
+		try{
+		}catch(Exception e){
+			
+		}
+	}
+}
